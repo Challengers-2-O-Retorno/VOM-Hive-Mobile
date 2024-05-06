a099c98775c910762490a1052e2396e55ad67813
gutolive09 <gutolive01@gmail.com>
2024-05-06 07:48:07 -0300

    padding HomeBanner ajustado

--- src/Components/HomeBanner.tsx
+++ src/Components/HomeBanner.tsx
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
     height: 400,
     position: "relative",
     paddingHorizontal: 20,
-    paddingVertical: 40,
+    paddingVertical: 15,
   },
   robot: {
     position: "absolute",
