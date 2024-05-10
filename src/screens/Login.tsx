6342c1446494b1eebe1f320db027d1066fff349f
gutolive09 <gutolive01@gmail.com>
2024-05-10 16:20:29 -0300

    form de login reestilizado

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,60 +1,77 @@
-import { StyleSheet, TextInput, Pressable, Text, View } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet, TextInput, Pressable, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
 
 export default function Login() {
+  const [user, setUser] = useState("");
+  const [pass, setPass] = useState("");
+
   return (
-    <SafeAreaView style={styles.main} >
-      <View style={styles.inputContainer} >
-      <TextInput
-        style={styles.input} 
-        placeholder='usuario'
-        
-        />
-        <TextInput
-        style={styles.input} 
-        placeholder='usuario'
-        secureTextEntry={true}
-        
-        />
-        <Pressable style={styles.btn} >
-          <Text style={styles.btnText} >Enviar</Text>
+    <SafeAreaView style={styles.main}>
+      <View style={styles.inputContainer}>
+        <View>
+          <Text style={styles.inputLabel} >Usuário</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="usuario"
+            value={user}
+            onChangeText={(value) => setUser(value)}
+          />
+        </View>
+        <View>
+          <Text style={styles.inputLabel} >Senha</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="usuario"
+            secureTextEntry={true}
+            value={pass}
+            onChangeText={(value) => setPass(value)}
+          />
+        </View>
+
+        <Pressable style={styles.btn}>
+          <Text style={styles.btnText}>Enviar</Text>
         </Pressable>
       </View>
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-  main:{
+  main: {
     backgroundColor: "#D88318",
-    height:"100%",
+    height: "100%",
     paddingVertical: 50,
   },
-  inputContainer:{
+  inputContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    gap: 40
+    gap: 40,
   },
-  input:{
+  input: {
     backgroundColor: "#fff",
-    color:"#000",
+    color: "#000",
     fontSize: 20,
     width: 300,
     padding: 15,
-    borderRadius: 35
+    borderRadius: 35,
   },
-  btn:{
-    borderColor: "#fff",
-    borderWidth: 4,
+  inputLabel:{
+    paddingLeft: 25,
+    fontWeight: "bold",
+    fontSize: 15
+  },
+  btn: {
+    backgroundColor: "#fff",
     paddingVertical: 10,
     paddingHorizontal: 50,
     borderRadius: 35,
   },
-  btnText:{
+  btnText: {
     fontSize: 20,
-    color: "#fff",
-    fontWeight: "bold"
-  }
-})
+    color: "#000",
+    fontWeight: "bold",
+  },
+});
