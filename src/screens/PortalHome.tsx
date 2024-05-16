7c9c9ca8c6f8c4164499b21dea7c441ba4e0d5fe
gutolive09 <gutolive01@gmail.com>
2024-05-16 15:31:03 -0300

    finalmente a porra da flatlist renderiza

--- src/screens/PortalHome.tsx
+++ src/screens/PortalHome.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, Text, View, FlatList, Image } from "react-native";
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
@@ -21,7 +21,7 @@ export default function PortalHome() {
       };
 
       c.push(campaign);
-      // console.log(campaign);
+      console.log(campaign);
     });
     setCampaigns(c);
     console.log(campaigns);
@@ -32,15 +32,16 @@ export default function PortalHome() {
   }, []);
 
   return (
-    <View>
+    <View style={styles.main}>
       <Text>Reveja suas ultimas campanhas</Text>
       <View style={styles.container}>
         <FlatList
           data={campaigns}
           renderItem={({ item, index }) => {
             return (
-              <View key={index} style={{ height: 100 }}>
-                <Text style={styles.item}>{item.category} aqui</Text>
+              <View key={index} style={styles.campaign}>
+                <Image width={60} height={60} source={{ uri: item.logo }} />
+                <Text style={styles.item}>{item.name}</Text>
               </View>
             );
           }}
@@ -51,6 +52,9 @@ export default function PortalHome() {
 }
 
 const styles = StyleSheet.create({
+  main: {
+    height: "100%",
+  },
   container: {
     height: 100,
     flex: 1,
@@ -60,5 +64,8 @@ const styles = StyleSheet.create({
     padding: 20,
     fontSize: 15,
     marginTop: 5,
+  },
+  campaign: {
+    height: 100,
   },
 });
